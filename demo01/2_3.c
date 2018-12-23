f51268d1b80a0ad41dfc61e08e30273ddedd9b82
bruce <bruce@liao.com>
2018-12-23 23:53:23 +0800

    update demo01

---
+++ demo01/2_3.c
@@ -0,0 +1,10 @@
+/*
+ * %d时格式化打印的占位符。
+ */
+#include <stdio.h>
+
+int main()
+{
+  printf("%d\n",2+3);
+  return 0;
+}
---
+++ demo01/star.c
@@ -0,0 +1,17 @@
+/*
+ * 每行打印和当前行数对应个数的*
+ */
+
+#include <stdio.h>
+
+int main()
+{
+ /*
+  printf("*\n");
+  printf("* *\n");
+  printf("* * * \n");
+  printf("* * * * \n");
+  */
+  printf("*\n* *\n* * *\n* * * *\n");
+  return 0;
+}
