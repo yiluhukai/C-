20f2aaa44b5bdb7c4b5f0371afd6dd82420cc583
root <root@debian>
2019-12-20 01:11:05 +0800

    fix code

---
+++ uc/project_01/k_add.c
@@ -0,0 +1,6 @@
+int k_add(int a,int b){
+	return a+b;
+}
+int k_sub(int a,int b){
+	return a-b;
+}
---
+++ uc/project_02/kmath/mux.c
@@ -0,0 +1,6 @@
+int k_div(int a,int b){
+	return a*b;
+}
+int k_mux(int a,int b){
+	return a/b;
+}
