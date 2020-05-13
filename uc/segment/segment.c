03e329335ca62c6a990fb08c1758e4759ee3c494
李俊杰 <lijunjie@lijunjiedeMac-mini.local>
2020-05-13 22:16:32 +0800

    fix bugs

--- uc/segment/segment.c
+++ uc/segment/segment.c
@@ -1,5 +1,6 @@
 /*
- *	端错误的产生
+ *	端错误的产生:对虚拟地址访问的时候找不到或者没有权限访问对应的
+ *	物理地址。
  */
 
 #include <stdio.h>
@@ -9,8 +10,8 @@ int main(void){
 	int * p ; // 野指针指向的内容是不确定的，有可能发生端错误
 	printf("%p\n",p);
 	//int * p=&var_i;
-	//p=(unsigned int)0x00003000;
-	*p=300;
+	p=(unsigned int)0x00003000;
+	//*p=300;
 	printf("%d\n",*p); 
 	return 0;
 }
