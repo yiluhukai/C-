c100c2128f92a0d845b8b917c30410c5e2cfaf4a
yiluhuakai <yiluhuakai@debian>
2020-06-06 00:27:24 +0800

    fix bugs

--- uc/file_message/file_message.c
+++ uc/file_message/file_message.c
@@ -9,7 +9,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <pwd.h>
+#include <grp.h>
 int main (int argc,char* argv[]){
 	const char* path = (const char *)argv[1];
 	struct stat st;
@@ -21,5 +22,11 @@ int main (int argc,char* argv[]){
 	printf("number of inode:%lu\n",st.st_ino);
 	printf("size of file;%lu\n",st.st_size);
 	printf("number of  hard link:%ld\n",st.st_nlink);
+	printf("uid of user:%u,username: %s\n",st.st_uid,getpwuid(st.st_uid)->pw_name);
+	//
+	printf("gid of user:%u,username:%s\n",st.st_gid,\
+		getgrgid(st.st_gid)->gr_name);
+	printf("access time of last:%ld\n",st.st_atim.tv_sec);
+	printf("access time of last:%ld\n",st.st_atime);
 	return 0;
 }
